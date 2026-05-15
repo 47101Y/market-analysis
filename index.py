ab411aa4ec2b13cfc7667cc048c8e17c364d4b68
47101Y <2824939061@qq.com>
2026-05-15 10:59:17 +0800

    2

--- index.py
+++ index.py
@@ -14,12 +14,31 @@ new_df = pd.read_csv('每日成交额TOP50新增股票.csv')
 weak_df = pd.read_csv('弱转强.csv')
 strong_df = pd.read_csv('强者恒强.csv')
 
-
-
-
 # =========================
-# 强者恒强详情字典
+#弱转强详情字典
 # =========================
+
+weak_detail = defaultdict(list)
+
+for _, row in weak_df.iterrows():
+
+    date = row['日期']
+
+    stock_info = f"""
+股票代码：{row['股票代码']}
+名称：{row['名称']}
+行业：{row['行业']}
+成交金额（亿）：{row['成交金额（亿）']}
+
+T日收盘价：{row['T日收盘价']}
+T-1日收盘价：{row['T-1日收盘价']}
+
+T日涨跌幅：{row['T日涨跌幅']}
+T+1日涨跌幅：{row['T+1日涨跌幅']}
+T+2日涨跌幅：{row['T+2日涨跌幅']}
+"""
+
+    weak_detail[date].append(stock_info)
 
 # =========================
 # 强者恒强详情字典
@@ -118,11 +137,17 @@ bar.add_js_funcs(f"""
 
 var strongData = {dict(strong_detail)};
 
+var weakData = {dict(weak_detail)};
+
 chart_{bar.chart_id}.on('click', function(params) {{
 
-    if(params.seriesName === '强者恒强'){{
+    var date = params.name;
 
-        var date = params.name;
+    // =====================
+    // 强者恒强
+    // =====================
+
+    if(params.seriesName === '强者恒强'){{
 
         var stocks = strongData[date];
 
@@ -130,6 +155,29 @@ chart_{bar.chart_id}.on('click', function(params) {{
 
             alert(
                 "【" + date + " 强者恒强股票】\\n\\n"
+                + stocks.join("\\n========================\\n")
+            );
+
+        }} else {{
+
+            alert("无数据");
+
+        }}
+
+    }}
+
+    // =====================
+    // 弱转强
+    // =====================
+
+    if(params.seriesName === '弱转强'){{
+
+        var stocks = weakData[date];
+
+        if(stocks){{
+
+            alert(
+                "【" + date + " 弱转强股票】\\n\\n"
                 + stocks.join("\\n========================\\n")
             );
 
